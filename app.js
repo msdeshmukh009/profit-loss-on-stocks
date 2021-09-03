9ce9d143533541457e62950d1d3253b4a271cb82
msdeshmukh009 <deshmukhmahesh009@gmail.com>
2021-09-03 12:17:18 +0530

    format: comments deleted

--- app.js
+++ app.js
@@ -9,9 +9,6 @@ var inputForm = document.forms[0];
 var formDiv = document.querySelector(".forms");
 var gifs = document.querySelector(".gif")
 
-// const profitOutput='<div>"You gained "'+percentProfit+'"% your total profit is ₹'+profit+'"</div><img width="100%" height="200px"  src="img/undraw_Investing_re_bov7.svg" alt="image">'
-//const lossOutput='<div>"You lost "'+percentLoss+'"% your total loss is ₹'+loss+'"</div><img width="100%" height="200px"  src="img/undraw_feeling_blue_4b7q.svg" alt="image"></img>'
-
  inputForm.addEventListener("submit",(e)=>{
      e.preventDefault();
      formDiv.classList.remove("happyTheme");
@@ -28,7 +25,6 @@ var gifs = document.querySelector(".gif")
                 var profit = ((cp-pp)*qnt).toFixed(2);
                 var percentProfit =(((cp-pp)*100)/pp).toFixed(2);
                 console.log(profit,percentProfit)
-                // gifs.style.display="block"
                 outputMsgDiv.innerHTML=`<div style="font-weight: bolder">You gained `+percentProfit+`% your total profit is ₹`+profit+` </div><img width="100%" height="200px"  src="img/undraw_Investing_re_bov7.svg" alt="image">`;
 
                 if(percentProfit>50){
